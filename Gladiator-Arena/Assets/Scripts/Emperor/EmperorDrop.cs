3bdc5da83a249eabbec82106faa9764df0b45932
Francisco Dominguez <fdominguez@itba.edu.ar>
2024-05-14 22:39:16 +0100

    Drop mechanic done

---
+++ Gladiator-Arena/Assets/Scripts/Emperor/EmperorDrop.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmperorDrop : MonoBehaviour
+{
+    public GameObject[] emperorItems;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        emperorItemDrop(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void emperorItemDrop()
+    {
+        int randomNumber;
+        for(int i = 0; i < emperorItems.Length; i++){
+            randomNumber = Random.Range(-30, 30);
+            Instantiate(emperorItems[i], transform.position + new Vector3(randomNumber, 0, 0), Quaternion.identity);
+        }   
+    }
+}
