57bf48a50bf6745c32667e8e02f002764ad16661
jmpalha <72129145+jmpalha@users.noreply.github.com>
2024-05-31 18:01:47 +0100

    rounds

--- Gladiator-Arena/Assets/Scripts/Core/CoreComponnents/Stats.cs
+++ Gladiator-Arena/Assets/Scripts/Core/CoreComponnents/Stats.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Stats : CoreComponent
 {
     [SerializeField] private float maxHealth;
-    private float currenthealth;
+    public float currenthealth;
 
     //[SerializeField] private HealthBar _healthBar;
 
@@ -26,12 +27,18 @@ public class Stats : CoreComponent
         if (currenthealth <= 0 )
         {
             currenthealth = 0;
-            Debug.Log("Dead");
+            Death();
         }
     }
 
     public void IncreaseHealth(float amount)
     {
         currenthealth = Mathf.Clamp(currenthealth + amount, 0, maxHealth);
+    }
+
+    public void Death()
+    {
+
+        GM.RemoveLife();
     }
 }
---
+++ Gladiator-Arena/Assets/Scripts/GM.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class GM
+{
+    public static int Lives1 = 3;
+    public static int Lives2 = 3;
+
+
+    public static void RemoveLife()
+    {
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+
+        Debug.Log(players[0].name);
+        Debug.Log(players[1].name);
+
+        if (players[0].name == "Player")
+        {
+            Stats stats = players[0].GetComponentInChildren<Stats>();
+
+            if(stats.currenthealth == 0 && Lives1 > 0)
+            {
+                Lives1--;
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+        else if (players[0].name == "Player2")
+        {
+            Debug.Log("okok");
+            Stats stats = players[0].GetComponentInChildren<Stats>();
+
+            if (stats.currenthealth == 0 && Lives2 > 0)
+            {
+                Lives2--;
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+        else if(players[1].name == "Player")
+        {
+
+            Stats stats = players[1].GetComponentInChildren<Stats>();
+
+            if (stats.currenthealth == 0 && Lives1 > 0)
+            {
+                Lives1--;
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+        else if (players[1].name == "Player2")
+        {
+
+            Stats stats = players[1].GetComponentInChildren<Stats>();
+
+
+            if (stats.currenthealth == 0 && Lives2 > 0)
+            {
+                Lives2--;
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+
+        if (Lives1 == 0)
+        {
+            Lives1 = 3;
+            Lives2 = 3;
+            SceneManager.LoadScene("Player2Wins");
+        }
+        else if(Lives2 == 0)
+        {
+            Lives1 = 3;
+            Lives2 = 3;
+            SceneManager.LoadScene("Player1Wins");
+        }
+    }
+}
